d21ca48982f36b567b6089474054c52ee2187d0c
deeeslv <andreantoniosilvaa@gmail.com>
2025-10-30 23:18:13 -0300

    Validação de usuário, novidade para mim. PRECISO REFAZER 100% SOZINHO.

---
+++ 007.js
@@ -0,0 +1,26 @@
+function validarUsuario(usuario){
+    let erros = []
+
+    if (usuario.nome.length < 2) erros.push("Nome inválido")
+    if (!usuario.email.includes("@")) erros.push("E-mail inválido")
+    if (usuario.senha.length < 6) erros.push("Senha inválida")
+    if (usuario.idade < 18) erros.push("Apenas maiores de 18!")
+
+    return {
+        valido: erros.length === 0,
+        erros: erros
+    }
+} 
+
+let usuarioTeste = {
+    nome: "A",
+    email: "semarroba",
+    senha: "123", 
+    idade: 15
+}
+
+let res = validarUsuario(usuarioTeste)
+
+console.log(res)
+//Acabei não conseguindo fazer sozinho, mas preciso refazer o exercício para entender melhor a lógica.
+//É simples, mas preciso praticar mais.
