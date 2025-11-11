f9fe117804731fd6834fee96d802afcae8fdc544
deeeslv <andreantoniosilvaa@gmail.com>
2025-11-10 22:56:32 -0300

    Criação de objetos

---
+++ 006.js
@@ -0,0 +1,11 @@
+function analisarTexto(texto){
+    return{ 
+        quantidadeDeCaracteres: texto.length,
+        contemJavaScript: texto.includes("JavaScript"),
+        textoMaiusculo: texto.toUpperCase(),
+    }
+}
+
+let res = analisarTexto("Estou estudando JavaScript!")
+
+console.log(res)
